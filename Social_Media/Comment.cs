b53376a7337020aa1f8d67c02026314b5089068b
KovaDav <kovacs.david990420@gmail.com>
2023-07-31 19:04:13 +0200

    comment class created, with moderate function

---
+++ Social_Media/Comment.cs
@@ -0,0 +1,34 @@
+﻿namespace Social_Media;
+
+public class Comment
+{
+    private string Message { get; set; }
+    public Guid Id { get; init; }
+    public DateTime DateOfCreation { get; init; }
+    private Post PostItBelongsTo { get; init; }
+    public bool Moderated { get; private set; }
+
+    public Comment(string message, Post postitbelongsto)
+    {
+        if (message.Length == 0)
+        {
+            throw new ArgumentException();
+        }
+        Message = message;
+        PostItBelongsTo = postitbelongsto;
+        Id = Guid.NewGuid();
+        DateOfCreation = DateTime.Now;
+        Moderated = false;
+    }
+
+    public void ModerateComment()
+    {
+        Moderated = true;
+    }
+    
+    public void ModerateComment(string message)
+    {
+        Message = message;
+        Moderated = true;
+    }
+}
