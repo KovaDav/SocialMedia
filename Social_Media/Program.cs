864a9db065bc0d0322e08071526dfc3abf936b62
KovaDav <kovacs.david990420@gmail.com>
2023-07-31 19:26:53 +0200

    Program.cs and test created/filled

--- Social_Media/Program.cs
+++ Social_Media/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Social_Media;
 
-Console.WriteLine("Hello, World!");
+Page page = new Page("title", "description");
+Post post = new Post("titleofpost","messageofpost",page,TypeOfPost.Regular);
+Comment comment1 = new Comment("messgeofcomment1", post);
+Comment comment2 = new Comment("messgeofcomment2", post);
+
+post.AddComment(comment1);
+post.AddComment(comment2);
+
+
+comment1.ModerateComment();
+
+
+
+page.AddPost(post);
+
+
+Console.WriteLine(page.ListOfModeratedComments("titleofpost"));
